f8132d35b40c33a663286d02168ac2c3cb072600
Bharath Kumar <bharathkumarkarthick@gmail.com>
2025-02-09 20:49:40 +0200

    footer landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@components/Hero";
 import { Cards } from "./components/Cards";
+import { Footer } from "./components/Footer";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Cards />
+      <Footer />
     </>
   );
 }
